0948cb5599e4d1e682bd427a7076ee3a71672b40
amanbains2911 <64704147+amanbains2911@users.noreply.github.com>
2020-06-04 16:55:53 -0400

    Initial commit

---
+++ src/test/java/com/aman/doctor/api/DoctorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.doctor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
